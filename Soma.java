fab9f630253dffc1f821aa54cab2a70e75411c4a
Matheus Valentim <114266242+NegaoValente@users.noreply.github.com>
2022-10-03 13:44:18 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main (String[] args) throws Exception {
+        
+
+        Scanner numero= new Scanner(System.in);
+       
+        
+        System.out.print("Informe um número:");
+        float numeral = numero.nextFloat();
+        Float number = numero.nextFloat();
+
+        final Float resultado =  number+ numeral;
+
+        System.out.print("A soma é :" +resultado);
+         
+    }
+}
